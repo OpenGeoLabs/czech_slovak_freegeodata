f4f1ccee7b62d2445a5bbd7a2395fda0f6648ff3
ruz76 <jan.ruzicka@cybele.space>
2021-04-03 10:17:42 +0200

    Translations.

--- i18n/GeoData_cs_CZ.ts
+++ i18n/GeoData_cs_CZ.ts
@@ -4,29 +4,54 @@
 <context>
     <name>GeoData</name>
     <message>
-        <location filename="../Geo_Data.py" line="176"/>
+        <location filename="../Geo_Data.py" line="181"/>
         <source>&amp;GeoData</source>
         <translation>&amp;GeoData</translation>
     </message>
     <message>
-        <location filename="../Geo_Data.py" line="164"/>
+        <location filename="../Geo_Data.py" line="169"/>
         <source>Browse data sources</source>
         <translation>Procházet datové zdroje</translation>
     </message>
     <message>
-        <location filename="../Geo_Data_dialog.py" line="227"/>
+        <location filename="../Region_dialog.py" line="168"/>
         <source>Error</source>
         <translation>Chyba</translation>
     </message>
     <message>
         <location filename="../Geo_Data_dialog.py" line="227"/>
         <source>Can not find web browser to open page about</source>
-        <translation>Nemohu najít prohlížeč k zobrazení stránky o aplikaci</translation>
+        <translation type="obsolete">Nemohu najít prohlížeč k zobrazení stránky o aplikaci</translation>
     </message>
     <message>
-        <location filename="../Geo_Data_dialog.py" line="221"/>
+        <location filename="../Geo_Data_dialog.py" line="270"/>
         <source>This functionality requires RUIAN plugin</source>
         <translation>Pro tuto funkci potřebujete RUIAN plugin</translation>
+    </message>
+    <message>
+        <location filename="../Region_dialog.py" line="105"/>
+        <source>Unable to read coordinate transformations definition file.</source>
+        <translation>Nemohu načíst definice transformací.</translation>
+    </message>
+    <message>
+        <location filename="../Region_dialog.py" line="181"/>
+        <source>Warning</source>
+        <translation>Varování</translation>
+    </message>
+    <message>
+        <location filename="../Region_dialog.py" line="168"/>
+        <source>Unable to read grids definition file.</source>
+        <translation>Nemohu načíst definici gridu.</translation>
+    </message>
+    <message>
+        <location filename="../Region_dialog.py" line="74"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../Region_dialog.py" line="74"/>
+        <source>You have to restart QGIS to apply all settings.</source>
+        <translation>Musíte restartovat QGIS, aby se všechna nastavení aplikovala.</translation>
     </message>
 </context>
 <context>
@@ -54,6 +79,34 @@ The plugin allows also to open RUIAN plugin dialog for import data from RUIAN se
         <translation>Plugin umožňuje přidat vybrané vrstvy do mapy. Pokud jsou tyto vrstvy zároveň v podobě služby (WMS/XYZ) pak jsou přidány do panelu Prohlížeče
 
 Plugin umožňuje spustit dialogové okno pluginu RUIAN pro import data z RUIAN.</translation>
+    </message>
+    <message>
+        <location filename="../Geo_Data_dialog_base.ui" line="113"/>
+        <source>Source Options</source>
+        <translation>Možnosti zdroje</translation>
+    </message>
+</context>
+<context>
+    <name>Region</name>
+    <message>
+        <location filename="../Region_dialog_base.ui" line="14"/>
+        <source>Region</source>
+        <translation>Region</translation>
+    </message>
+    <message>
+        <location filename="../Region_dialog_base.ui" line="26"/>
+        <source>Select your region. It will set default settings suitable for your region.</source>
+        <translation>Vyberte region. Nastavíte tak parametry vhodné pro něj. </translation>
+    </message>
+    <message>
+        <location filename="../Region_dialog_base.ui" line="45"/>
+        <source>SVK</source>
+        <translation>SVK</translation>
+    </message>
+    <message>
+        <location filename="../Region_dialog_base.ui" line="58"/>
+        <source>CZE</source>
+        <translation>CZE</translation>
     </message>
 </context>
 </TS>
