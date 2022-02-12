a6c3d0d145d7d47921201a8c1020ca873ff60e2b
misopasko <miso.pasko@gmail.com>
2022-02-12 16:31:51 +0100

    Added new layers

---
+++ data_sources/CUZK_RUIAN_MC/source.py
@@ -0,0 +1,21 @@
+from .. source import Source
+import os
+from qgis.core import QgsVectorLayer, QgsMessageLog
+
+class MC(Source):
+
+    def get_vector(self, extent, EPSG):
+        url = 'https://geoportal.cuzk.cz/zakazky/SPH/SPH_SHP_JTSK.zip'
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'data', 'SPH_SHP_JTSK.zip')
+        self.download_data(url, path, "ČUZK Ruian MC")
+        path = '/vsizip/' + os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'data', 'SPH_SHP_JTSK.zip') + '/JTSK/SPH_MC.shp'
+        vector = QgsVectorLayer(path, "MC", "ogr")
+        vector.loadNamedStyle(os.path.dirname(__file__) + '/data/style.qml')
+        if not vector.isValid():
+            QgsMessageLog.logMessage("Vrstvu " + path + " se nepodařilo načíst", "GeoData")
+            return None
+        else:
+            return vector
+
+    def get_raster(self, extent, EPSG):
+        return None
